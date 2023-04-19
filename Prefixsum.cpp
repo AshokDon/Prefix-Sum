e114562e71f34761ba0b5e5fa56a124f21e9323c
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-04-19 12:01:12 +0530

    Create Prefixsum.cpp

---
+++ Prefixsum.cpp
@@ -0,0 +1,39 @@
+#print sum in range quaries 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        long long A[n];
+        for(int i = 0 ; i < n ; i++)cin >> A[i];
+        //finding prefix sum
+        for(int i = 1 ; i < n ; i++){
+            A[i]+=A[i-1];
+        }
+        int Q;
+        cin >> Q;
+        while(Q--){
+            int l , r;
+            cin >> l >> r;
+            //convert to zero base index
+            l--;
+            r--;
+            //long long sum = 0;
+            if(l!=0){
+                cout<<A[r]-A[l-1]<<"\n";
+            }
+            else{
+                cout<<A[r]<<"\n";
+            }
+           //cout<<sum<<"\n";
+        }
+    }
+}
